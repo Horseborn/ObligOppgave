c0853f39c154ea953c9c4c9151647c79f28c014c
Horseborn <79469928+Horseborn@users.noreply.github.com>
2021-05-28 17:18:08 +0200

    Initial commit

---
+++ model.js
@@ -0,0 +1,51 @@
+const model = {
+    input:{
+        movieWrite: {
+            movieInput: '',
+
+        },
+
+      
+
+    },
+    movieView: {
+        movies: [
+            {
+            id: 001,
+            name: 'Midsommar',
+            haveSeen: false
+
+            },
+            {
+                id: 002,
+                name: '',
+                haveSeen: false
+            },
+            {
+                id: 003,
+                name: '',
+                haveSeen: false
+            },
+            {
+                id: 004,
+                name: '',
+                haveSeen: false
+            },
+            {
+                id: 005,
+                name: '',
+                haveSeen
+            },
+            {
+                id: 006,
+                name: '',
+                haveSeen: false
+            }
+
+
+
+        ]
+    }
+
+    
+}
